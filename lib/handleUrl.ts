df878a3d1d7851db8e2e4630f0511e995d0a67f7
Mika Reich <mikareich0@gmail.com>
2023-07-28 22:40:10 +0000

    Herpderp, shoulda check if it does really compile.

--- lib/handleUrl.ts
+++ lib/handleUrl.ts
@@ -4,16 +4,19 @@ const protocolRegex = /^(\w+):\/\//
 
 // append http:// if no protocol is provided and test it
 export const formatUrl = (url: string) => {
+  console.log('formatUrl', url)
+
   // check if url has a protocol and test it
   if (protocolRegex.test(url)) {
     // check if protocol is http or https
     const protocol = url.match(protocolRegex)![1]
     if (protocol !== 'http' && protocol !== 'https')
       throw new Error('Invalid protocol')
+  } else {
+    // append http:// if no protocol is provided and test it
+    url = `http://${url}`
   }
 
-  // append http:// if no protocol is provided and test it
-  url = `http://${url}`
   if (!isValidUrl(url)) throw new Error('Invalid Url')
 
   return url
@@ -23,7 +26,7 @@ export const protocolRelativeUrl = (url: string) => {
   return protocolRegex.test(url) ? url.replace(protocolRegex, '') : url
 }
 
-export const generateUrl = ( shortSlug: string) => {
-  const {VERCEL_URL} = process.env
-  return `${VERCEL_URL}/${shortSlug}`
+export const generateUrl = (shortSlug: string) => {
+  const { VERCEL_URL } = process.env
+  return formatUrl(`${VERCEL_URL}/${shortSlug}`)
 }
